0169676633661bab256c1c0e5e36bc04e913166b
Miguel <miguelfernando477@gmail.com>
2023-02-07 16:39:20 -0600

    display names

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-function makeTwoPlayerGame(){
-  document.createElement()
+function displayNames() {
+  let player1Name = document.querySelector('#player1Name')
+  let player2Name = document.querySelector('#player2Name')
+  let displayNames = document.querySelector('#displayNames')
+
+  displayNames.innerHTML =player1Name.value + ' (X)'+ ' VS '  + player2Name.value + " (O)";
 }
-
-const twoPlayer = document.getElementById("Two-Player")
-twoPlayer.addEventListener('click', makeTwoPlayerGame )
-
-
-
