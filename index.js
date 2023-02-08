976e9914c4f016fb8b895b790881bf5212a51f64
Miguel <miguelfernando477@gmail.com>
2023-02-07 20:46:03 -0600

    stlye done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+//Display Names
+
 function displayNames() {
   let player1Name = document.querySelector('#player1Name')
   let player2Name = document.querySelector('#player2Name')
   let displayNames = document.querySelector('#displayNames')
-
   displayNames.innerHTML =player1Name.value + ' (X)'+ ' VS '  + player2Name.value + " (O)";
 }
+
+const gameState = {
+  players: ['x', 'o'],
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ]
+}
+
+const spaces = Array.from(document.getElementsByClassName('Spaces'))
