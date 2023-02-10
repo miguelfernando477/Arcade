605e2dfc3e47e4b233c06a4cb098ea87cb6dc407
Miguel <miguelfernando477@gmail.com>
2023-02-09 19:04:04 -0600

    draw and restart function

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-//Display Names
+let spaces = document.getElementsByClassName("Space");
+const gameState = {
+  players: ["X", "O"],
+  board: [null, null, null, null, null, null, null, null, null],
+};
+
+currentPlayer = gameState.players[0];
+
+//Display Names & Start Game
 
 function displayNames() {
-  let player1Name = document.querySelector('#player1Name')
-  let player2Name = document.querySelector('#player2Name')
-  let displayNames = document.querySelector('#displayNames')
-  displayNames.innerHTML = player1Name.value + ' (X)'+ ' VS '  + player2Name.value + " (O)";
-  const whosTurn = document.querySelector('#whosTurn')
-  const winText = document.querySelector('#winText')
-  let spaces = document.getElementsByClassName('Space')
+  let player1Name = document.querySelector("#player1Name");
+  let player2Name = document.querySelector("#player2Name");
+  let displayNames = document.querySelector("#displayNames");
+  let whosTurn = document.querySelector('#whosTurn')
+  displayNames.innerHTML =
+  player1Name.value + " (X)" + " VS " + player2Name.value + " (O)";
+  const winText = document.querySelector("#winText");
   for (var i = 0; i < spaces.length; i++) {
-    spaces[i].addEventListener('click', makeMove);
+    spaces[i].addEventListener("click", makeMove);
   }
 }
 
-
-
-
 //Game Play
 
-const gameState = {
-  players: ['X', 'O'],
-  board: [null, null, null , null, null, null, null, null, null]
-}
-
-currentPlayer = gameState.players[0] || gameState.players[1]
-
-
-function makeMove(evt){
-  whosTurn.innerHTML = `${currentPlayer}'s Turn`;
+function makeMove(evt) {
+  whosTurn.innerHTML = `${currentPlayer}'s Turn` 
   const spot = evt.target.id;
-  console.log(spot);
-  if (!gameState.board[spot]){
+  console.log(spot)
+  if (!gameState.board[spot]) {
     gameState.board[spot] = currentPlayer;
-    evt.target.innerHTML = currentPlayer
-    if(playerWon()){
+    evt.target.innerHTML = currentPlayer;
+    if (playerWon()) {
       winText.innerHTML = `${currentPlayer}'s Wins!`;
-      return
+      
+      return;
     }
-    currentPlayer = currentPlayer === gameState.players[0] ? gameState.players[1] : gameState.players[0]
+      if (currentPlayer === gameState.players[0]){
+        currentPlayer = gameState.players[1]
+    } else {
+      currentPlayer = gameState.players[0]
+    } 
+  }
+  if (
+    !playerWon() &&
+    !gameState.board.includes(null)
+  ) {
+    winText.innerHTML = `Draw`;
   }
 }
 
@@ -47,47 +55,78 @@ function makeMove(evt){
 // [3 , 4 , 5]
 // [6 , 7 . 8]
 
-function playerWon(){
-  if(gameState.board[0] === currentPlayer){
-    if(gameState.board[1] === currentPlayer && gameState.board[2] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
+function playerWon() {
+  if (gameState.board[0] === currentPlayer) {
+    if (
+      gameState.board[1] === currentPlayer &&
+      gameState.board[2] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
       return true;
     }
-    if(gameState.board[3] === currentPlayer && gameState.board[6] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
+    if (
+      gameState.board[3] === currentPlayer &&
+      gameState.board[6] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
       return true;
     }
-    if(gameState.board[4] === currentPlayer && gameState.board[8] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
-      return true;
-    }
-  }
-  if(gameState.board[4] === currentPlayer){
-    if(gameState.board[1] === currentPlayer && gameState.board[7] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
-      return true;
-    }
-    if(gameState.board[3] === currentPlayer && gameState.board[5] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
-      return true;
-    }
-    if(gameState.board[2] === currentPlayer && gameState.board[6] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
+    if (
+      gameState.board[4] === currentPlayer &&
+      gameState.board[8] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
       return true;
     }
   }
-  if(gameState.board[8] === currentPlayer){
-    if(gameState.board[2] === currentPlayer && gameState.board[5] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
+  if (gameState.board[4] === currentPlayer) {
+    if (
+      gameState.board[1] === currentPlayer &&
+      gameState.board[7] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
       return true;
     }
-    if(gameState.board[6] === currentPlayer && gameState.board[7] === currentPlayer){
-      console.log(`${currentPlayer} wins`)
+    if (
+      gameState.board[3] === currentPlayer &&
+      gameState.board[5] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
+      return true;
+    }
+    if (
+      gameState.board[2] === currentPlayer &&
+      gameState.board[6] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
       return true;
     }
   }
-  
+  if (gameState.board[8] === currentPlayer) {
+    if (
+      gameState.board[2] === currentPlayer &&
+      gameState.board[5] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
+      return true;
+    }
+    if (
+      gameState.board[6] === currentPlayer &&
+      gameState.board[7] === currentPlayer
+    ) {
+      console.log(`${currentPlayer} wins`);
+      return true;
+    }
+  }
 }
 
-
-
+//Restart
+function restart(){
+ console.log(gameState.board)
+    for (var i = 0; i < gameState.board.length; i++){
+    gameState.board[i] = null
+    document.getElementById(i).innerHTML = ""
+    }
+    winText.innerHTML = ``
+    
+}
