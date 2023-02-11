548e06600d9b49c0aa67ce99e9aeeb27cd87ec76
Miguel <miguelfernando477@gmail.com>
2023-02-10 22:27:22 -0600

    fully functional

--- index.js
+++ index.js
@@ -1,47 +1,37 @@
+//Global Variables
+let isSinglePlayer = false
 let spaces = document.getElementsByClassName("Space");
-let currentPlayer
+let currentPlayer;
 const gameState = {
   players: ["X", "O"],
   board: [null, null, null, null, null, null, null, null, null],
 };
 
-x = Math.floor(Math.random() *2) 
-console.log(x)
+//Randomly Chooses Who Starts
+
+x = Math.floor(Math.random() * 2);
+
 if (x === 0) {
-  currentPlayer = gameState.players[0]
+  currentPlayer = gameState.players[0];
 } else {
-  currentPlayer = gameState.players[1]
+  currentPlayer = gameState.players[1];
 }
 
-//Display Names & Start Game
+//Two Player
 
 function displayNames() {
+  const winText = document.querySelector("#winText");
   let player1Name = document.querySelector("#player1Name");
   let player2Name = document.querySelector("#player2Name");
   let displayNames = document.querySelector("#displayNames");
   displayNames.innerHTML =
-  player1Name.value + " (X)" + " VS " + player2Name.value + " (O)";
-  let whosTurn = document.querySelector('#whosTurn')
-  whosTurn.innerHTML = `${currentPlayer}'s Turn` 
-  const winText = document.querySelector("#winText");
+    player1Name.value + " (X)" + " VS " + player2Name.value + " (O)";
+  let whosTurn = document.querySelector("#whosTurn");
+  whosTurn.innerHTML = `${currentPlayer}'s Turn`;
+
   for (var i = 0; i < spaces.length; i++) {
     spaces[i].addEventListener("click", makeMove);
   }
-}
-
-//Game Play
-function disableButtons(){let buttons = document.querySelectorAll(".Space")
-buttons.forEach(button => {
- button.removeEventListener('click', makeMove, false)
-})
-}
-
-function enableButtons(){
-let buttons = document.querySelectorAll(".Space")
-     buttons.forEach(button => {
-      button.addEventListener('click', makeMove)
-     })
-
 }
 
 function makeMove(evt) {
@@ -51,34 +41,136 @@ function makeMove(evt) {
     evt.target.innerHTML = currentPlayer;
     if (playerWon()) {
       winText.innerHTML = `${currentPlayer}'s Wins!`;
-      disableButtons();
       return;
     }
-    if (currentPlayer === gameState.players[0]){
-      currentPlayer = gameState.players[1]
+    if (currentPlayer === gameState.players[0]) {
+      currentPlayer = gameState.players[1];
     } else {
-      currentPlayer = gameState.players[0]
-    } 
-    whosTurn.innerHTML = `${currentPlayer}'s Turn` 
+      currentPlayer = gameState.players[0];
+    }
+    whosTurn.innerHTML = `${currentPlayer}'s Turn`;
   }
-  if (
-    !playerWon() &&
-    !gameState.board.includes(null)
-  ) {
+  if (!playerWon() && !gameState.board.includes(null)) {
     winText.innerHTML = `Draw`;
   }
 }
+
+//Enable & Disable Clicks Two Player
+
+function disableClicks() {
+  let buttons = document.querySelectorAll(".Space");
+  buttons.forEach((button) => {
+    button.removeEventListener("click", makeMove, false);
+  });
+}
+
+function enableClicks() {
+  let buttons = document.querySelectorAll(".Space");
+  buttons.forEach((button) => {
+    button.addEventListener("click", makeMove);
+  });
+}
+
+//Enable & Disable Clicks Single Player
+
+function disableSingleClicks() {
+  let buttons = document.querySelectorAll(".Space");
+  buttons.forEach((button) => {
+    button.removeEventListener("click", makeSingleMove, false);
+  });
+}
+
+function enableSingleClicks() {
+  let buttons = document.querySelectorAll(".Space");
+  buttons.forEach((button) => {
+    button.addEventListener("click", makeSingleMove);
+  });
+}
+
 // Single Player
 
-function singlePlayer(){
+function singlePlayer() {
+  isSinglePlayer = true
+  for (var i = 0; i < gameState.board.length; i++) {
+    gameState.board[i] = null;
+    document.getElementById(i).innerHTML = "";
+  }
+  winText.innerHTML = ``;
+  whosTurn.innerHTML = ``;
+  disableClicks();
+  whosTurn.innerHTML = "";
+  let displayNames = document.querySelector("#displayNames");
+  displayNames.innerHTML = "";
+  document.querySelector("#startSingle").style.display = "block";
+  document.querySelector("#startTwo").style.display = "none";
+  document.querySelector("#player2").style.display = "none";
+}
+
+function displaySingleName() {
+  if (isSinglePlayer && currentPlayer === gameState.players[1]) {
+    makeCompMove();
+  }
   let player1Name = document.querySelector("#player1Name");
-  let player2Name = document.querySelector("#player2Name");
-  let player2 = document.querySelector("#player2Name");
-  player2Name.hide;
-  player2.hide;
   let displayNames = document.querySelector("#displayNames");
   displayNames.innerHTML =
-  player1Name.value + " (X)" + " VS " + "Computer (O)";
+    player1Name.value + " (X)" + " VS " + "Computer" + " (O)";
+  whosTurn.innerHTML = `${currentPlayer}'s Turn`;
+  for (var i = 0; i < spaces.length; i++) {
+    spaces[i].addEventListener("click", makeSingleMove);
+  }
+}
+
+function makeSingleMove(evt) {
+  const spot = evt.target.id;
+  if (gameState.board[spot]) {
+    return;
+  }
+  gameState.board[spot] = currentPlayer;
+  evt.target.innerHTML = currentPlayer;
+  if (playerWon()) {
+    winText.innerHTML = `${currentPlayer}'s Wins!`;
+    return;
+  }
+  if (currentPlayer === gameState.players[0]) {
+    currentPlayer = gameState.players[1];
+  } else {
+    currentPlayer = gameState.players[0];
+  }
+  whosTurn.innerHTML = `${currentPlayer}'s Turn`;
+
+  if (!playerWon() && !gameState.board.includes(null)) {
+    winText.innerHTML = `Draw`;
+  }
+  setTimeout(() => {
+    makeCompMove()
+  }, 1500);
+  
+  console.log(gameState.board);
+}
+
+function makeCompMove() {
+  
+  x = Math.floor(Math.random() * 9);
+  while (gameState.board[x] !== null) {
+    x = Math.floor(Math.random() * 9);
+  }
+  if (!gameState.board[x]) {
+    gameState.board[x] = currentPlayer;
+    document.getElementById(`${x}`).innerHTML = currentPlayer;
+    if (playerWon()) {
+      winText.innerHTML = `${currentPlayer}'s Wins!`;
+      return;
+    }
+    if (currentPlayer === gameState.players[0]) {
+      currentPlayer = gameState.players[1];
+    } else {
+      currentPlayer = gameState.players[0];
+    }
+    whosTurn.innerHTML = `${currentPlayer}'s Turn`;
+  }
+  if (!playerWon() && !gameState.board.includes(null)) {
+    winText.innerHTML = `Draw`;
+  }
 }
 
 //Winning Combos
@@ -93,21 +185,24 @@ function playerWon() {
       gameState.board[1] === currentPlayer &&
       gameState.board[2] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
     if (
       gameState.board[3] === currentPlayer &&
       gameState.board[6] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
     if (
       gameState.board[4] === currentPlayer &&
       gameState.board[8] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
   }
@@ -116,21 +211,24 @@ function playerWon() {
       gameState.board[1] === currentPlayer &&
       gameState.board[7] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
     if (
       gameState.board[3] === currentPlayer &&
       gameState.board[5] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
     if (
       gameState.board[2] === currentPlayer &&
       gameState.board[6] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
   }
@@ -139,35 +237,43 @@ function playerWon() {
       gameState.board[2] === currentPlayer &&
       gameState.board[5] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
     if (
       gameState.board[6] === currentPlayer &&
       gameState.board[7] === currentPlayer
     ) {
-      console.log(`${currentPlayer} wins`);
+      disableSingleClicks();
+      disableClicks();
       return true;
     }
   }
 }
 
 //Restart
-function restart(){
- console.log(gameState.board)
-    for (var i = 0; i < gameState.board.length; i++){
-    gameState.board[i] = null
-    document.getElementById(i).innerHTML = ""
-    }
-    winText.innerHTML = ``
-    whosTurn.innerHTML = `` 
-    x = Math.floor(Math.random() *2) 
-console.log(x)
-if (x === 0) {
-  currentPlayer = gameState.players[0]
-} else {
-  currentPlayer = gameState.players[1]
-}
-    whosTurn.innerHTML = `${currentPlayer}'s Turn` 
-    enableButtons()
+
+function restart() {
+  for (var i = 0; i < gameState.board.length; i++) {
+    gameState.board[i] = null;
+    document.getElementById(i).innerHTML = "";
+  }
+  winText.innerHTML = ``;
+  whosTurn.innerHTML = ``;
+  x = Math.floor(Math.random() * 2);
+
+  if (x === 0) {
+    currentPlayer = gameState.players[0];
+  } else {
+    currentPlayer = gameState.players[1];
+  }
+  whosTurn.innerHTML = `${currentPlayer}'s Turn`;
+  if (isSinglePlayer) {
+  enableSingleClicks();
+  }
+  enableClicks();
+  if (isSinglePlayer && currentPlayer === gameState.players[1]) {
+    makeCompMove();
+  }
 }
